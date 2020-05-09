e4962347f8ed2897fff70a8c339b8b3373d086e7
dehio3 <20110646+dehio3@users.noreply.github.com>
2020-05-09 19:07:52 +0900

    add proxy

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: '/' + process.env.GITHUB_REPOSITORY.split('/')[1]
+  publicPath: '/' + process.env.GITHUB_REPOSITORY.split('/')[1],
+  devServer: {
+    proxy: 'https://itunes.apple.com'
+  }
 }
