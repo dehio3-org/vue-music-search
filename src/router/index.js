2e2d3c0d6866c969ef69205d42553bb1aa531074
dehio3 <tomohide@MBP-2.local>
2020-05-09 13:43:18 +0900

    add result page

--- src/router/index.js
+++ src/router/index.js
@@ -4,19 +4,16 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/result/:keyword',
+    name: 'result',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Result.vue')
   }
 ]
 
---
+++ src/views/Result.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">検索結果</h1>
+        <p>検索キーワード：{{ $route.params.keyword }}</p>
+      </v-col>
+
+      <v-col cols="12">
+        <v-card color="#385F73" dark>
+          <v-card-title class="headline" v-text="title"></v-card-title>
+
+          <v-card-subtitle v-text="artistName"></v-card-subtitle>
+
+          <v-card-actions>
+            <v-btn text>Listen Now</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col v-for="(item, i) in items" :key="i" cols="12">
+        <v-card :color="item.color" dark>
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <div>
+              <v-card-title class="headline" v-text="item.title"></v-card-title>
+
+              <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+            </div>
+
+            <v-avatar class="ma-3" size="125" tile>
+              <v-img :src="item.src"></v-img>
+            </v-avatar>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "アルバムタイトル",
+      artistName: "アーティスト名",
+      image: ""
+    };
+  }
+};
+</script>
